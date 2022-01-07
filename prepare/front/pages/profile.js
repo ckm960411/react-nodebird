26b5c309cd52a9cc407889c611033eeb5d193136
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-07 12:50:19 +0900

    antd 다운, _app.js 생성, Head 태그 적용

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,9 +1,15 @@
-import AppLayout from "../components/AppLayout"
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
   return (
-    <AppLayout>내 프로필</AppLayout>
-  )
-}
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>내 프로필</AppLayout>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
