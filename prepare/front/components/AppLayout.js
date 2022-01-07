2e30170a3d511a01c9db2e206e2e49c6473a98a3
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-07 13:18:14 +0900

    antd 로 반응형 grid 만들기
    Row, Col 컴포넌트 사용

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,22 +1,44 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Input, Menu, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
         <Menu.Item>
-          <Link href="/"><a>노드버드</a></Link>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          <Link href="/profile"><a>프로필</a></Link>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          <Link href="/signup"><a>회원가입</a></Link>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.zerocho.com"
+            target="_blank"
+            rel="noreferrer noopener" // target _blank 의 보안을 위해서 적어줌
+          >
+            Made by ZeroCho
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
