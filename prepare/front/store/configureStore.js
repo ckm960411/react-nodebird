312a642115f1af4da74e66971f1a409c71a953de
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-08 20:58:18 +0900

    Redux-devtools 도입
    redux-devtools-extension 설치

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,10 +1,15 @@
 import { createWrapper } from "next-redux-wrapper"
-import { createStore } from "redux"
+import { applyMiddleware, compose, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import reducer from '../reducers/index'
 
 
 const configureStore = () => {
-  const store = createStore(reducer)
+  const middlewares = [] // saga 나 thunk 가 들어오게 될 자리
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares)) // 제품용
+    : composeWithDevTools(applyMiddleware(...middlewares)) // 개발용, 히스토리를 남김
+  const store = createStore(reducer, enhancer)
   return store
 }
 
