103491d535aef266e3f0d4239f0f2920e2cafc20
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-09 23:15:55 +0900

    redux-saga 설치와 설정, rootSaga 설정

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,15 +1,23 @@
 import { createWrapper } from "next-redux-wrapper"
 import { applyMiddleware, compose, createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
+import createSagaMiddleware from "@redux-saga/core"
 import reducer from '../reducers/index'
+import rootSaga from '../sagas'
 
+const loggerMiddleware = ({ dispatch, getState}) => next => action => {
+  console.log(action)
+  return next(action)
+}
 
 const configureStore = () => {
-  const middlewares = [] // saga 나 thunk 가 들어오게 될 자리
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [sagaMiddleware, loggerMiddleware] // saga 나 thunk 가 들어오게 될 자리
   const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares)) // 제품용
     : composeWithDevTools(applyMiddleware(...middlewares)) // 개발용, 히스토리를 남김
   const store = createStore(reducer, enhancer)
+  store.sagaTask = sagaMiddleware.run(rootSaga)
   return store
 }
 
