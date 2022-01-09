b7188d716dfd0a291004884929dd4eb12f11c375
ckm960411 <78003237+ckm960411@users.noreply.github.com>
2022-01-10 08:31:46 +0900

    saga 이펙트 사용

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
-import { all, call, fork, put, take } from 'redux-saga/effects'
+import { all, call, fork, put, takeEvery, takeLatest, delay } from 'redux-saga/effects'
 
 function logInAPI(data) {
   return axios.post('/api/login', data)
 }
 function* logIn(action) {
   try {
-    const result = yield call(logInAPI, action.data)
+    // const result = yield call(logInAPI, action.data)
+    yield delay(1000)
     yield put({
       type: 'LOG_IN_SUCCESS',
       data: result.data,
@@ -24,7 +25,8 @@ function logOutAPI() {
 }
 function* logOut() {
   try {
-    const result = yield call(logOutAPI)
+    // const result = yield call(logOutAPI)
+    yield delay(1000)
     yield put({
       type: 'LOG_OUT_SUCCESS',
       data: result.data,
@@ -42,7 +44,8 @@ function addPostAPI(data) {
 }
 function* addPost(action) {
   try {
-    const result = yield call(addPostAPI, action.data)
+    // const result = yield call(addPostAPI, action.data)
+    yield delay(1000)
     yield put({
       type: 'ADD_POST_SUCCESS',
       data: result.data,
@@ -56,13 +59,15 @@ function* addPost(action) {
 }
 
 function* watchLogIn() {
-  yield take('LOG_IN_REQUEST', logIn)
+  // 마우스가 더블클릭 되는 등의 이벤트가 발생하면
+  // 요청을 여러번 수행하지 않고 마지막 요청을 받아들임
+  yield takeLatest('LOG_IN_REQUEST', logIn)
 }
 function* watchLogOut() {
-  yield take('LOG_OUT_REQUEST', logOut)
+  yield takeLatest('LOG_OUT_REQUEST', logOut)
 }
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST', addPost)
+  yield takeLatest('ADD_POST_REQUEST', addPost)
 }
 
 export default function* rootSaga() {
